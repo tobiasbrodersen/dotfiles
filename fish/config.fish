55e413a3a1f7283087e17be093cf213088953995
Tobias Brodersen <tobias@tobs.io>
2024-01-29 16:56:16 +0100

    init

---
+++ fish/config.fish
@@ -0,0 +1,21 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# ~/.local.bin
+set -x PATH ~/.local/bin/ $PATH
+
+# Setting PATH for Python 3.11
+# The original version is saved in /Users/toby/.config/fish/config.fish.pysave
+set -x PATH "/Library/Frameworks/Python.framework/Versions/3.11/bin" "$PATH"
+
+# brew
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# neovim
+alias v="nvim"
+alias vi="nvim"
+alias vim="nvim"
+
+# kubernetes
+alias k="kubectl"
