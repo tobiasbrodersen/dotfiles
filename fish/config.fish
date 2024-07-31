1388b023df959161feaf9130ccfde5537edd139b
Tobias Brodersen <tobias@tobs.io>
2024-07-31 10:48:10 +0200

    remove lazyvim for now, remove key mapping used for old work laptop

--- fish/config.fish
+++ fish/config.fish
@@ -2,15 +2,13 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# My work MAC has iso layout, map the section key to tilde
-alias isoansi="sudo hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000064,"HIDKeyboardModifierMappingDst":0x700000035}]}'"
-
 # ~/.local.bin
 set -x PATH ~/.local/bin/ $PATH
 
 # Setting PATH for Python 3.11
 # The original version is saved in /Users/toby/.config/fish/config.fish.pysave
 set -x PATH "/Library/Frameworks/Python.framework/Versions/3.11/bin" "$PATH"
+set -x PATH "/Users/toby/Library/Python/3.12/bin" "$PATH"
 
 # brew
 eval "$(/opt/homebrew/bin/brew shellenv)"
